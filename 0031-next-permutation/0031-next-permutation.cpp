d3aabb1966cdc5ce556a9f079a4bc77b3202ac19
USHA BHARATHI L <ushabharathi0421@gmail.com>
2026-06-16 23:00:13 +0530

    Time: 0 ms (100%), Space: 15.7 MB (32.34%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        if(n <= 1) return;
+
+        int j = -1;
+
+
+        for(int i = n-2; i >= 0; i--){
+            if(nums[i] < nums[i+1]){
+                j = i;
+                break;
+            }
+        } 
+
+    
+        if(j == -1){
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+
+        for(int i = n-1; i > j; i--){
+            if(nums[i] > nums[j]){
+                swap(nums[i], nums[j]);
+                break;
+            }
+        }
+
+    
+        reverse(nums.begin() + j + 1, nums.end());
+    }
+};
