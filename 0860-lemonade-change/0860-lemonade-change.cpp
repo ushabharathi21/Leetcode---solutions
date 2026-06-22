4e81a2ec9aea9892bd06e1b8a15f223ffefe4c77
USHA BHARATHI L <ushabharathi0421@gmail.com>
2026-06-22 19:23:45 +0530

    Time: 0 ms (100%), Space: 87.2 MB (75.02%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& arr) {
+        int n = arr.size();
+        int five = 0, ten = 0;
+        for(int i=0;i<n;i++){
+            if(arr[i] == 5){
+                five = five+1;
+            }else if(arr[i] == 10){
+                if(five){
+                    five -=1;
+                    ten = ten+1;
+                }else
+                  return false;
+            }else{
+                if(ten && five){
+                    ten -= 1;
+                    five -= 1;
+                }else if(five >= 3){
+                    five -= 3;
+                }else
+                  return false;
+            }
+        }
+
+        return true;
+    }
+};
