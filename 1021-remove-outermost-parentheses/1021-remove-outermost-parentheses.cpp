66f2d53bf3c7ab7b170b93bab9ed6daa5df895eb
USHA BHARATHI L <ushabharathi0421@gmail.com>
2026-07-02 21:50:54 +0530

    Time: 0 ms (100%), Space: 9 MB (24%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string ans;
+        int depth = 0;
+        for(char c : s){
+            if(c=='('){
+                if(depth > 0)
+                 ans += c;
+                depth++;
+            }else{
+
+                if(c==')'){
+                    depth--;
+                    if(depth>0)
+                     ans += c;
+                
+                }
+            }
+        }
+        return ans;
+    }
+};
