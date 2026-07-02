b54fae5bd0e333410c79fcdd2410de99af3ae019
USHA BHARATHI L <ushabharathi0421@gmail.com>
2026-07-02 21:42:52 +0530

    Time: 0 ms (100%), Space: 20.7 MB (40.68%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int jumps = 0, l = 0, r = 0;
+        int n =nums.size();
+        while(r<n-1){
+            int farthest = r;
+            for(int ind = l;ind<=r;ind++){
+                farthest = max(ind+nums[ind], farthest);
+            }
+            l = r+1;
+            r = farthest;
+            jumps++;
+        }
+        return jumps;
+    }
+};
