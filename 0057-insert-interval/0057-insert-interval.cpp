abda65d85f74b5cdbe1e7484ae0512c435de7147
USHA BHARATHI L <ushabharathi0421@gmail.com>
2026-07-06 19:36:02 +0530

    Time: 0 ms (100%), Space: 21.8 MB (47.96%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& arr, vector<int>& newInterval) {
+        vector<vector<int>> res;
+        int i=0;
+        int n = arr.size();
+        while(i<n && arr[i][1] < newInterval[0]){
+            res.push_back(arr[i]);
+            i++;
+        }
+        while(i<n && arr[i][0] <= newInterval[1]){
+            newInterval[0] = min(newInterval[0], arr[i][0]);
+            newInterval[1] = max(newInterval[1], arr[i][1]);
+            i++;
+        }
+        res.push_back(newInterval);
+        while(i<n){
+            res.push_back(arr[i]);
+            i++;
+        }
+        return res;
+    }
+};
