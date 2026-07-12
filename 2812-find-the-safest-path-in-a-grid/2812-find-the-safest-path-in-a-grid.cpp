d34031befab1df69d497c516e4a070c857365202
USHA BHARATHI L <ushabharathi0421@gmail.com>
2026-07-12 20:34:22 +0530

    Time: 258 ms (89.03%), Space: 122 MB (82.24%) - LeetHub

--- 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.cpp
+++ 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.cpp
@@ -1,60 +1,71 @@
 class Solution {
-    static constexpr int dirs[4][2] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
-
 public:
-    int maximumSafenessFactor(vector<vector<int>>& A) {
-        if (A[0][0] || A.back().back())
-            return 0;
+    vector<int> roww = {0,0,-1,1};
+    vector<int> coll = {-1,1,0,0};
 
-        int n = A.size();
+    void bfs(vector<vector<int>>& grid,vector<vector<int>>& score,int n) {
         queue<pair<int, int>> q;
-
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++)
-                if (A[i][j])
+        //finding all the thieves
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++){
+                if(grid[i][j]) {
+                    score[i][j] = 0;
                     q.push({i, j});
-
-        while (q.size()) {
-            auto [i, j] = q.front();
+                }
+            }
+        }
+        //bfs from every thief to find distance from nearest thief
+        while(!q.empty()){
+            auto t = q.front();
             q.pop();
 
-            int v = A[i][j];
+            int x = t.first, y = t.second;
+            int s = score[x][y];
 
-            for (auto& d : dirs) {
-                int x = i + d[0];
-                int y = j + d[1];
+            for(int i =0; i < 4; i++){
+                int newX = x + roww[i];
+                int newY = y + coll[i];
 
-                if (min(x, y) >= 0 && max(x, y) < n && !A[x][y]) {
-                    A[x][y] = v + 1;
-                    q.push({x, y});
+                if(newX >= 0 && newX < n && newY >= 0 && newY < n && score[newX][newY] > 1 + s) { 
+
+                    score[newX][newY] = 1 + s;
+                    q.push({newX, newY});
                 }
             }
         }
+    }
 
+    int maximumSafenessFactor(vector<vector<int>>& grid) {
+        int n = grid.size();
+        if(grid[0][0] || grid[n - 1][n - 1]) return 0;
 
+        vector<vector<int>> score(n,vector<int>(n,INT_MAX));
+        bfs(grid, score, n);
+        vector<vector<bool>> vis(n, vector<bool>(n, false));
 
-        priority_queue<tuple<int, int, int>> pq;
+        priority_queue<pair<int,pair<int,int>>> pq;
+        pq.push({score[0][0], {0,0}});
 
-        pq.push({A[0][0], 0, 0});
-
-        while (pq.size()) {
-            auto [sf, i, j] = pq.top();
+        while(!pq.empty()){
+            auto temp = pq.top().second;
+            auto safe = pq.top().first;
             pq.pop();
 
-            if (i == n - 1 && j == n - 1)
-                return sf - 1;
+            if(temp.first == n - 1 && temp.second == n - 1) return safe;
+            vis[temp.first][temp.second] = true;
 
-            for (auto& d : dirs) {
-                int x = i + d[0];
-                int y = j + d[1];
+            for(int i = 0; i < 4; i++) {
+                int newX = temp.first + roww[i];
+                int newY = temp.second + coll[i];
 
-                if (min(x, y) >= 0 && max(x, y) < n && A[x][y] > 0) {
-                    pq.push({min(sf, A[x][y]), x, y});
-                    A[x][y] *= -1;
+                if(newX >= 0 && newX < n && newY >= 0 && newY < n && !vis[newX][newY]){
+                    int s = min(safe, score[newX][newY]);
+                    pq.push({s, {newX, newY}});
+                    vis[newX][newY] = true;
                 }
             }
         }
 
-        return A.back().back() - 1;
+        return -1;
     }
 };
