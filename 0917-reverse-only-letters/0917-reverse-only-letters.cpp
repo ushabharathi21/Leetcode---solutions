09b80676505ff5f902daf5ecb3548feb2b90e676
USHA BHARATHI L <ushabharathi0421@gmail.com>
2026-07-15 21:24:56 +0530

    Time: 0 ms (100%), Space: 8.1 MB (54.82%) - LeetHub

---
+++ 0917-reverse-only-letters/0917-reverse-only-letters.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+        int start = 0, end = s.size()-1;
+        while(start < end){
+            if(isalpha(s[start]) && isalpha(s[end])){
+                swap(s[start], s[end]);
+                start++;
+                end--;
+            }else if(!isalpha(s[start])){
+                start++;
+            }else{
+                end--;
+            }
+        }
+
+        return s;
+    }
+};
