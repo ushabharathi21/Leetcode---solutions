55333d5f48679a661fd1e80dd4475a60defc7244
USHA BHARATHI L <ushabharathi0421@gmail.com>
2026-09-11 09:19:35 +0530

    Time: 0 ms (100%), Space: 8.4 MB (24.79%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        int p=0, ans = 0;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='(') p++;
+            else if(s[i]==')') p--;
+
+            ans = max(ans,p);
+        }
+        return ans;
+    }
+};
