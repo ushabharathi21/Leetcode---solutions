de9dfde6be46cb7fda7e276d411c5fc233fdb4fb
USHA BHARATHI L <ushabharathi0421@gmail.com>
2026-09-13 19:32:23 +0530

    Time: 0 ms (100%), Space: 60.9 MB (51.48%) - LeetHub

---
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
@@ -0,0 +1,42 @@
+class Solution {
+private:
+    TreeNode* first;
+    TreeNode* middle;
+    TreeNode* last;
+    TreeNode* prev;
+
+    void inorder(TreeNode* root) {
+        if(root == NULL) return;
+
+        inorder(root->left);
+
+        if(prev != NULL && root->val < prev->val) {
+            if(first == NULL) {
+                first = prev;
+                middle = root;
+            }
+            else {
+                last = root;
+            }
+        }
+
+        prev = root;
+
+        inorder(root->right);
+    }
+
+public:
+    void recoverTree(TreeNode* root) {
+        first = middle = last = NULL;
+        prev = NULL;
+
+        inorder(root);
+
+        if(first && last) {
+            swap(first->val, last->val);
+        }
+        else if(first && middle) {
+            swap(first->val, middle->val);
+        }
+    }
+};
