31ed2889f25d386312d68d05094a45a8a9b79b57
USHA BHARATHI L <ushabharathi0421@gmail.com>
2026-09-15 19:24:02 +0530

    Time: 0 ms (100%), Space: 8.7 MB (20.2%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
-    int climbStairs(int n) {
-        if(n==0 || n==1){
+    int f(int n, vector<int>& dp) {
+        if(n == 0 || n == 1)
             return 1;
-        }
-       int prev = 1, curr = 1;
-       for(int i=2;i<=n;i++){
-        int temp = curr;
-        curr = prev+curr;
-        prev = temp;
-       }
 
-       return curr;
+        if(dp[n] != -1)
+            return dp[n];
+
+        dp[n] = f(n-1, dp) + f(n-2, dp);
+
+        return dp[n];
+    }
+
+    int climbStairs(int n) {
+        vector<int> dp(n+1, -1);
+
+        return f(n, dp);
     }
 };
